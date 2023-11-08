68f27507cd0864b2434e1379d66e166ae61f858d
Manvitha-Mallireddy <113509596+Manvitha-Mallireddy@users.noreply.github.com>
2023-11-08 11:35:41 +0530

    Update AreaMain.java

--- src/AreaMain.java
+++ src/AreaMain.java
@@ -2,8 +2,12 @@
 public class AreaMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		AreaCircle c=new AreaCircle();
+		c.circle(7);
+		AreaRect r=new AreaRect();
+		r.rect(5,10);
+		AreaCuboid cd=new AreaCuboid();
+		cd.CalciCuboid(2,10,5);
 	}
 
 }
